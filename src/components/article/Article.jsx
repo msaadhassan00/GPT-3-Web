0316fc7a77a0a89fb7b1503fb72866fd4623d072
Muhammad Saad Hassan <88556409+saadhassan2002@users.noreply.github.com>
2022-08-15 01:33:55 +0500

    Blog Section Completed

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './article.css'
 
-const Article = () => {
+const Article = ({ imgUrl, date, text }) => {
   return (
-    <div>Article</div>
+    <div className='blog-container-article'>
+          <div className='blog-container-article-image'>
+              <img src={imgUrl} alt="blog_image" />
+          </div>
+          <div className='blog-container-article-content'>
+                <div>
+                  <p>{date}</p>
+                  <h3>{text}</h3>
+                </div>
+                <p>Read Full Article</p>
+          </div>
+    </div>
   )
 }
 
