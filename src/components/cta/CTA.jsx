85aff5e3e0d02a56809ea705451f0b379ca0f01c
Muhammad Saad Hassan <88556409+saadhassan2002@users.noreply.github.com>
2022-08-15 00:59:48 +0500

    CTA Component completed

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import './cta.css'
+import React from "react";
+import "./cta.css";
 
 const CTA = () => {
   return (
-    <div>CTA</div>
-  )
-}
+    <div className="cta">
+      <div className="cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className='cta-btn'>
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
+};
 
-export default CTA
+export default CTA;
